821376e67a139b83db08e5c807716eb15beb90ba
Camilo Castillo <npgcamilo@gmail.com>
2019-06-25 09:57:23 -0500

    changes on tree

--- lib/components/table-helisa/table-helisa-connect.component.d.ts
+++
@@ -1,9 +0,0 @@
-import { ColumnConfig } from "./table-helisa.interface";
-export declare class TableHelisaConnectComponent<T> {
-    page: number;
-    isLastPage: boolean;
-    isUsed: boolean;
-    constructor();
-    getBody(columnConfig: Array<ColumnConfig>, search: string): any;
-    nextPage(): number;
-}
--- lib/components/toast-helisa/toast-type.enum.d.ts
+++
@@ -1,5 +0,0 @@
-export declare enum ToastType {
-    DONE = "DONE",
-    ERROR = "ERROR",
-    INFO = "INFO"
-}
