1547a372e663301fc06f62374e3a7582e05192ce
Camilo Castillo <npgcamilo@gmail.com>
2019-06-25 11:35:44 -0500

    changes

---
+++ lib/components/date-helisa/date-helisa.component.d.ts
@@ -0,0 +1,8 @@
+import { OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+export declare class DateHelisaComponent implements OnInit {
+    placeholder: string;
+    dateFormControl: FormControl;
+    constructor();
+    ngOnInit(): void;
+}
